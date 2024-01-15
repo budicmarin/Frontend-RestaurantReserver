05504eda9d7fa2a9e6787cf02df04c58c1746d2f
budicmarin <budicmarin1@gmail.com>
2024-01-15 17:33:23 +0100

    Onsubmit

--- restaurantreserver/src/views/RegisterView.vue
+++ restaurantreserver/src/views/RegisterView.vue
@@ -4,39 +4,45 @@
       <div class="col-md-6">
         <h1 class="text-center mb-5">This is a register page</h1>
         <form @submit.prevent="onSubmit" class="form-row">
-         
-          <div class="form-group col-md-6">
-            <label for="inputName">Ime</label>
-            <input type="name" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Enter name">
+
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="inputName">Ime</label>
+                <input type="text" class="form-control" id="inputName" v-model="formData.name" placeholder="Enter name" required>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="inputSureName">Prezime</label>
+                <input type="text" class="form-control" id="inputSureName" v-model="formData.surname" placeholder="Enter surname" required>
+              </div>
+            </div>
           </div>
 
-          <div class="form-group col-md-6">
-            <label for="inputSureName">Prezime</label>
-            <input type="surename" class="form-control" id="inputSureName" aria-describedby="surenameHelp" placeholder="Enter surename">
-          </div>
-       
           <div class="form-group col-md-12">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" v-model="formData.email" placeholder="Enter email" required>
           </div>
 
           <div class="form-group col-md-12">
             <label for="examplePhone">Broj Mobitela</label>
-            <input type="phone" class="form-control" id="examplePhoneNumber" aria-describedby="phoneHelp" placeholder="Enter Broj Telefona">
+            <input type="tel" class="form-control" id="examplePhoneNumber" v-model="formData.phoneNumber" placeholder="Enter Broj Telefona" required>
           </div>
 
           <div class="form-group col-md-12">
-            <DatePickermodel />
+            <!-- Include the DatePickermodel component and pass any necessary props -->
+            <!-- <DatePickermodel /> -->
           </div>
 
           <div class="form-group col-md-12">
             <label for="exampleInputPassword1">Lozinka</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="form-control" id="exampleInputPassword1" v-model="formData.password" placeholder="Password" required>
           </div>
 
           <div class="form-group col-md-12">
             <label for="exampleInputPassword2">Ponovi Lozinku</label>
-            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+            <input type="password" class="form-control" id="exampleInputPassword2" v-model="formData.confirmPassword" placeholder="Password" required>
           </div>
 
           <div class="form-group col-md-12">
@@ -49,19 +55,27 @@
   </div>
 </template>
 
-
-
 <script>
-import DatePicker from 'vue-datepicker';
+import { defineComponent } from 'vue';
 
-export default {
-  components: {
-    DatePicker,
-  },
+export default defineComponent({
   data() {
     return {
-      yourDateModel: null, // or your initial date value
+      formData: {
+        name: '',
+        surname: '',
+        email: '',
+        phoneNumber: '',
+        password: '',
+        confirmPassword: '',
+      },
     };
   },
-};
+  methods: {
+    onSubmit() {
+      console.log('Registriran', this.formData);
+      // You can perform additional actions here, such as sending the form data to a server.
+    },
+  },
+});
 </script>
--- restaurantreserver/src/views/ReservationView.vue
+++ restaurantreserver/src/views/ReservationView.vue
@@ -1,38 +1,66 @@
 <template>
-  <div class="reservation">
-    <h1>Odabir stola</h1>
-    <form @submit.prevent="onSubmit">
-          <div>
-          <div class="form-group">
-            <label for="inputName">Ime</label>
-            <input type="name" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Enter name">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h1>Odabir stola</h1>
+        <form @submit.prevent="onSubmit">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="inputName">Ime</label>
+                <input type="text" class="form-control" id="inputName" v-model="formData.name" placeholder="Enter name" required>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="inputSureName">Prezime</label>
+                <input type="text" class="form-control" id="inputSureName" v-model="formData.surname" placeholder="Enter surname" required>
+              </div>
+            </div>
           </div>
-        
-          <div class="form-group">
-            <label for="inputSureName">Prezime</label>
-            <input type="surename" class="form-control" id="inputSureName" aria-describedby="surenameHelp" placeholder="Enter surename">
-          </div>
-        </div>
           <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" v-model="formData.email" placeholder="Enter email" required>
           </div>
           <div class="form-group">
             <label for="examplePhone">Broj Mobitela</label>
-            <input type="phone" class="form-control" id="examplePhoneNumber" aria-describedby="phoneHelp" placeholder="Enter Broj Telefona">
+            <input type="tel" class="form-control" id="examplePhoneNumber" v-model="formData.phoneNumber" placeholder="Enter phone number" required>
           </div>
           <div class="form-group">
             <label for="examplePersonNumber">Broj Osoba</label>
-            <input type="phone" class="form-control" id="examplePersonNumber" aria-describedby="personNumberHelp" placeholder="Enter Broj Osoba">
+            <input type="number" class="form-control" id="examplePersonNumber" v-model="formData.numberOfPeople" placeholder="Enter number of people" required>
           </div>
           <div>
-            <DatePickermodel  />
-           
+            <!-- Include the DatePickermodel component and pass any necessary props -->
+            <!-- <DatePickermodel /> -->
           </div>
-         
-
-
           <button type="submit" class="btn btn-primary mt-3">Rezerviraj</button>
         </form>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      formData: {
+        name: '',
+        surname: '',
+        email: '',
+        phoneNumber: '',
+        numberOfPeople: 0,
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('Registriran', this.formData);
+      // You can perform additional actions here, such as sending the form data to a server.
+    },
+  },
+});
+</script>
